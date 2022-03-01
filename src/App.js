b23b6993b7b7b24f7a00ca7b018e66bd16965776
HopeKayumov <umidjonkayumov2000@gmail.com>
2022-03-02 00:44:18 +0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "antd/dist/antd.css";
+import { Layout, Menu, Breadcrumb } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
-function App() {
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ height: "100vh" }}>
+      <Header className="header">
+        <div className="logo">Imsoft</div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          <Menu.Item key="1">Клиенты</Menu.Item>
+          <Menu.Item key="2">Деталь</Menu.Item>
+          <Menu.Item key="3">Модель</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider width={200} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            style={{ height: "100%", borderRight: 0 }}
+          >
+            <SubMenu key="sub1" icon={<UserOutlined />} title="Cправочник">
+              <Menu.Item key="1">Клиенты</Menu.Item>
+              <Menu.Item key="2">Деталь</Menu.Item>
+              <Menu.Item key="3">Модель</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: "0 24px 24px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>Панел</Breadcrumb.Item>
+            <Breadcrumb.Item>Справочник</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className="site-layout-background"
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
-}
+};
 
 export default App;
